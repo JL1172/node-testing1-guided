1083a6393f6b4d83cc7b43364093606bd369e4cf
Jacob Lang <jacoblang72@comcast.net>
2023-10-17 22:26:24 -0400

    finished test

--- car/car.js
+++ car/car.js
@@ -1,10 +1,20 @@
 // Build a Car class!
+
 class Car {
     constructor(make, model) {
-        this.make = make,
+        this.make = make
         this.model = model
+        this.odometer = 0
+    }
+    drive(distance) {
+        this.odometer += distance;
+    }
+    async driveAsync(distance) {
+        this.odometer += distance;
+        return this.odometer;
     }
 }
 
 module.exports = Car;
+
 
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,4 +1,5 @@
-const Car = require("./car")
+const Car = require("./car");
+
 function foo () {
     return "the foo";
 }
@@ -30,14 +31,38 @@ describe("Our Second Tests",() => {
 })
 
 describe("Our Third Tests",()=> {
+    let prius 
+beforeEach(()=> {
+    prius = new Car("toyota","prius")
+})
     test("[5]Car Class Testing",()=> {
         expect(Car).toBeDefined();
         expect(Car).toBeInstanceOf(Function)
     })
     test("[6]Has model and make",() => {
-        const prius = new Car("toyota","prius");
         expect(prius).toHaveProperty("make", "toyota");
         expect(prius).toHaveProperty("model", "prius")
-        expect(prius).toEqual({make :  "toyota", model : "prius"})
+        expect(prius).toMatchObject({make :  "toyota", model : "prius"})
+    })
+    test("[7]New cars start with the odometer at zero",()=> {
+        expect(prius).toHaveProperty("odometer",0);
+        expect(prius.odometer).toEqual(0); 
+        expect(prius).toMatchObject({odometer : 0})
+    })
+    test("[8]Cars have a drive method",() => {
+        expect(prius.drive).toBeDefined();
+        expect(prius.drive).toBe(Car.prototype.drive)
+    })
+    test("[9]Drive methods takes distance and increases odometer by that distance",()=> {
+        prius.drive(10);
+        expect(prius.odometer).toBe(10);
+        prius.drive(5);
+        expect(prius.odometer).toBe(15);
+    })
+    test("[10]Drive async method resolves the updated odometer",async()=> {
+        let updatedOdometer = await prius.driveAsync(7);
+        expect(updatedOdometer).toBe(7);
+        updatedOdometer = await prius.driveAsync(5);
+        expect(updatedOdometer).toBe(12); 
     })
 })
